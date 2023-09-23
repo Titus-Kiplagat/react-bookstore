ea706ce607fdd2486f94760a7ef1284499a4cd9b
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-09-23 12:03:16 +0300

    Add navigation routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
+import Navigation from './components/Navigation';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="w-11/12 mx-auto h-screen">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/categories" element={<BookForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
