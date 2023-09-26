3df8861c78929cbc5c6996868146ecdb00c511b7
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-09-26 10:55:10 +0300

    Setup slice of state of books

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  bookItems: [],
+};
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.bookItems.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      state.bookItems = state.bookItems.filter((book) => book.id !== action.payload);
+    },
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+export default booksSlice.reducer;
