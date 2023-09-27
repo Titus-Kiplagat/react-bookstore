02a1e0400e97d50caf4177dcb68f6e82668ad272
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-09-27 14:07:41 +0300

    Import Button component and dispatch remove Book action

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,24 +1,32 @@
+/* eslint-disable no-console */
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
+import Button from './Button';
 
-const Book = ({ book, onDelete }) => (
-  <div className="border p-3 mb-2 flex gap-5">
-    <p className="text-xl font-bold">{book.title}</p>
-    <button
-      type="button"
-      onClick={() => onDelete(book.id)}
-      className="bg-red-500 text-white px-2 py-1 rounded"
-    >
-      Delete
-    </button>
-  </div>
-);
+const Book = ({ book }) => {
+  const dispatch = useDispatch();
+  const { title, item_id: itemId } = book;
+  return (
+
+    <div className="border p-3 mb-2 flex gap-5">
+      <p className="text-xl font-bold">{title}</p>
+      <Button
+        type="button"
+        onClick={() => dispatch(removeBook(itemId))}
+        buttonStyles="bg-red-500 text-white px-2 py-1 rounded"
+      >
+        REMOVE
+      </Button>
+    </div>
+  );
+};
 
 Book.propTypes = {
-  book: PropTypes.exact({
-    id: PropTypes.number.isRequired,
+  book: PropTypes.shape({
+    item_id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
   }).isRequired,
-  onDelete: PropTypes.func.isRequired,
 };
 
 export default Book;
