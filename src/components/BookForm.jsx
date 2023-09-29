03511cf125f1a594d19ab32765b6f6bbdb47cb8c
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-09-29 09:31:00 +0300

    Add select category input field and post data to API functionality

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,11 +1,13 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
-import { addBook } from '../redux/books/booksSlice';
+import { v4 as uuid } from 'uuid';
+import { addBook, postBookApi } from '../redux/books/booksSlice';
 import Button from './Button';
 
 const BookForm = () => {
   const dispatch = useDispatch();
-  const [formData, setFormData] = useState({ title: '', author: '', category: 'Under construction' });
+  const { categories } = useSelector((state) => state.categories);
+  const [formData, setFormData] = useState({ title: '', author: '', category: '' });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -19,9 +21,13 @@ const BookForm = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const newBook = { ...formData, item_id: uuid() };
     if (validateFormData(formData)) {
-      dispatch(addBook(formData));
-      setFormData((prevState) => ({ ...prevState, title: '', author: '' }));
+      dispatch(addBook(newBook));
+      dispatch(postBookApi(newBook));
+      setFormData((prevState) => ({
+        ...prevState, title: '', author: '', category: '',
+      }));
     } else {
       // setMessage('Please fill all the fields');
     }
@@ -48,10 +54,22 @@ const BookForm = () => {
             name="author"
             required
             className="bg-transparent border-2 rounded-full py-1 px-6 text-[16px] leading-[22.4px] font-light placeholder:text-gray-500 text-black"
-            placeholder="Book Title"
+            placeholder="Book Author"
             value={author}
             onChange={handleChange}
           />
+          <select
+            name="category"
+            defaultValue="Category"
+            required
+            className="bg-transparent border-2 rounded-full py-1 px-6 text-[16px] leading-[22.4px] font-light placeholder:text-gray-500 text-black"
+            onChange={handleChange}
+          >
+            <option value="Category" disabled>Category</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>{category}</option>
+            )) }
+          </select>
           <Button type="submit">
             ADD BOOK
           </Button>
