093d66afce5cb890757db0bc8d1e95faa2e20ef8
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-09-29 09:28:55 +0300

    Add fetch data from API functioinality

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,18 +1,36 @@
-import { useSelector } from 'react-redux';
+/* eslint-disable no-console */
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import Book from './Book';
 import BookForm from './BookForm';
+import { getBooksApi } from '../redux/books/booksSlice';
 
 const BookList = () => {
+  const dispatch = useDispatch();
   const { bookItems } = useSelector((state) => state.books);
+
+  useEffect(() => {
+    dispatch(getBooksApi());
+  }, [dispatch]);
+
+  if (bookItems.length === 0) {
+    return (
+      <div>
+        <BookForm />
+        <div>
+          <h2 className="text-2xl font-bold my-3">Book List</h2>
+          <p className="text-xl font-bold">No Books Available</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
       <BookForm />
       <div>
         <h2 className="text-2xl font-bold my-3">Book List</h2>
-        {bookItems.map((book) => (
-          <Book key={book.item_id} book={book} />
-        ))}
+        {bookItems && bookItems.map((book) => <Book key={book.item_id} book={book} />)}
       </div>
     </div>
   );
