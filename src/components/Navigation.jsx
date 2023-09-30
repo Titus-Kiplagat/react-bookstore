25af1e90424bdc379a6ae2f034bd09bfcbd8c300
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-09-30 16:47:34 +0300

    Update styling and add user avatar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
 
 const Navigation = () => (
-  <nav className="py-4 uppercase flex items-center gap-20">
-    <h1 className="text-2xl font-bold text-blue-500">Bookstore CMS</h1>
-    <div className="text-gray-500">
-      <Link to="/" className="mr-5">
-        Books
-      </Link>
-      <Link to="/categories">
-        Categories
-      </Link>
+  <nav className="w-full bg-white px-20 py-4 flex items-center justify-between sticky top-0 border border-b-2 border-b-gray-100 font-montserrat">
+    <div className="flex items-center gap-20">
+      <h1 className="text-3xl font-bold text-[#0290ff]">Bookstore CMS</h1>
+      <div className="text-gray-500 text-sm tracking-[1.9px] uppercase">
+        <Link to="/" className="mr-20 hover:text-black">
+          Books
+        </Link>
+        <Link to="/categories" className="hover:text-black active">
+          Categories
+        </Link>
+      </div>
     </div>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="#0290ff" viewBox="0 0 24 24" strokeWidth={0} stroke="currentColor" className="w-10 h-10 border rounded-full cursor-pointer p-1">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+    </svg>
   </nav>
 );
 
